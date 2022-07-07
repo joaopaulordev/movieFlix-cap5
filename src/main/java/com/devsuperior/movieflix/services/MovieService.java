9253fbacd1f54b6066ce3a3485858d6ede4af11a
João Paulo Rodrigues <joaopaulo.dev@hotmail.com>
2022-07-07 16:45:20 -0300

    Final Project

---
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,47 @@
+package com.devsuperior.movieflix.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.MovieByGenreDTO;
+import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class MovieService {
+	
+	@Autowired
+	private MovieRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<MovieByGenreDTO> findByGenre(Long genreId, Pageable pageable){
+		genreId = (genreId == 0) ? null : genreId;
+		Page<Movie> page = repository.findByGenre(genreId, pageable);
+		return page.map(x -> new MovieByGenreDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Optional<Movie> obj = repository.findById(id);
+		Movie entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new MovieDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public List<ReviewDTO> findReviewsByMovieID(Long id) {
+		Optional<Movie> obj = repository.findById(id);
+		Movie entityMovie = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		
+		return entityMovie.getReviews().stream().map(x -> new ReviewDTO(x)).collect(Collectors.toList());
+	}
+}
